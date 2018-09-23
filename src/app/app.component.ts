7b90b4f9de8c11c286448e19d47d2ed685139ab8
carlossegovia <carlitossegovia1979@gmail.com>
2018-09-23 16:27:09 -0400

    -Se agregó al menú iconos. -Se cambiaron los nombres de las páginas.
    Signed-off-by: carlossegovia <carlitossegovia1979@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,31 @@
 import {Component, ViewChild} from '@angular/core';
 import {MenuController, Nav, Platform} from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import {HomePage} from '../pages/home/home';
 import {DenunciasPage} from "../pages/denuncias/denuncias";
 import {ListadoPage} from "../pages/listado/listado";
 import {MapaPage} from "../pages/mapa/mapa";
 import {LoginPage} from "../pages/login/login";
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
-  rootPage:any = LoginPage;
-  pages: Array<{component: any, nombre: string}> = [];
+  rootPage: any = LoginPage;
+  pages: Array<{ component: any, nombre: string }> = [];
 
   constructor(platform: Platform, statusBar: StatusBar,
               public menu: MenuController, splashScreen: SplashScreen) {
     //Las páginas que se mostrarán en el menú
     this.pages = [
-      {component: HomePage, nombre: 'Inicio'},
-      {component: DenunciasPage, nombre: 'Agregar denuncia'},
-      {component: ListadoPage, nombre: 'Listado de denuncias'},
-      {component: MapaPage, nombre: 'Mapa'},
-      {component: LoginPage, nombre: 'Salir'}
+      {component: HomePage, nombre: 'Inicio', icon: 'home'},
+      {component: DenunciasPage, nombre: 'Nueva Denuncia', icon: 'add'},
+      {component: ListadoPage, nombre: 'Mis Denuncias', icon: 'list'},
+      {component: MapaPage, nombre: 'Mapa de Denuncias', icon: 'locate'},
+      {component: LoginPage, nombre: 'Salir', icon: 'exit'}
 
     ];
 
@@ -35,10 +36,10 @@ export class MyApp {
     });
   }
 
-  abrirPagina(pagina){
+  abrirPagina(pagina) {
     //Se cierra el menú
     this.menu.close();
-  //Se usa setRoot para que no aparezca el botón de atrás
+    //Se usa setRoot para que no aparezca el botón de atrás
     this.nav.setRoot(pagina.component);
   }
 
